1b5e2718545e3daaef9210002ae9b90ef79baf8c
Avinash Kumar Singh <acavinashchauhan5@gmail.com>
2025-11-19 20:47:01 +0530

    adding terraform aws roboshop

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0005065ZXFJMJERYTTP"
+}
+
+variable "zone_name" {
+    default = "joindevops.fun"
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+    
+}
